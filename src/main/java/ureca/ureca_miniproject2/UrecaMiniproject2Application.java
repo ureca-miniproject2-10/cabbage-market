e71dbb50c11d6ae35a32c7dd9927dcf264b3a354
Jang-GO <jang8195@naver.com>
2025-05-08 14:11:53 +0900

    initial commit

---
+++ src/main/java/ureca/ureca_miniproject2/UrecaMiniproject2Application.java
@@ -0,0 +1,13 @@
+package ureca.ureca_miniproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrecaMiniproject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrecaMiniproject2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ureca/ureca_miniproject2/UrecaMiniproject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ureca.ureca_miniproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrecaMiniproject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
