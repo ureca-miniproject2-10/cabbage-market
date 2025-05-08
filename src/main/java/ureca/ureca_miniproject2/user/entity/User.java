bf3a51ada93d274c8c1f4eff0d6e354e979bf784
lbk6661 <lbk6661@lbk6661ui-MacBookPro.local>
2025-05-08 15:32:15 +0900

    Add user package

---
+++ src/main/java/ureca/ureca_miniproject2/user/entity/User.java
@@ -0,0 +1,11 @@
+package ureca.ureca_miniproject2.user.entity;
+
+public class User {
+
+    private Integer userId; // pk
+    private String username; // 아이디
+    private String password; // 비밀번호
+    private String profile_image; // 이미지 경로
+    private String name; // 이름
+
+}
---
+++ src/main/java/ureca/ureca_miniproject2/user/entity/UserRole.java
@@ -0,0 +1,9 @@
+package ureca.ureca_miniproject2.user.entity;
+
+
+public class UserRole {
+
+    private Integer roleId;
+    private String role; // ROLE_USER , ROLE_ADMIN , ROLE_WARNING , 일반 유저 , 관리자 , 제한적인 유저
+
+}
