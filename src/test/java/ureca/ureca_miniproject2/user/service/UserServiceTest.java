c22f878b854656ca6ff5396fcb028e049cd6439b
lbk00 <lbk6661@gmail.com>
2025-05-09 15:40:01 +0900

    test 코드

---
+++ src/test/java/ureca/ureca_miniproject2/user/service/UserServiceTest.java
@@ -0,0 +1,29 @@
+package ureca.ureca_miniproject2.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class UserServiceTest {
+
+    @Test
+    void userList() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void registerUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+}
