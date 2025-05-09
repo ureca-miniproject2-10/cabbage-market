01411f68acd9bb4fde79f72121871102441dee8d
Jang-GO <jang8195@naver.com>
2025-05-09 13:30:50 +0900

    [FEAT] : 게시글 컨트롤러 작성

--- src/main/java/ureca/ureca_miniproject2/util/response/SuccessMessages.java
+++ src/main/java/ureca/ureca_miniproject2/util/response/SuccessMessages.java
@@ -7,12 +7,13 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum SuccessMessages {
-    POST_REGISTER_SUCCESS(HttpStatus.CREATED, "게시글 등록에 성공하였습니다.");
+    // POST 관련 성공 메시지
+    POST_CREATE(HttpStatus.CREATED.value(), "게시글이 성공적으로 생성되었습니다."),
+    POST_FIND(HttpStatus.OK.value(), "게시글 조회에 성공했습니다."),
+    POST_FIND_ALL(HttpStatus.OK.value(), "전체 게시글 조회에 성공했습니다."),
+    POST_UPDATE(HttpStatus.OK.value(), "게시글 수정에 성공했습니다."),
+    POST_DELETE(HttpStatus.OK.value(), "게시글 삭제에 성공했습니다.");
 
-    private final HttpStatus httpStatus;
+    private final int httpStatus;
     private final String message;
-
-    public int getHttpStatus() {
-        return this.httpStatus.value();
-    }
 }
