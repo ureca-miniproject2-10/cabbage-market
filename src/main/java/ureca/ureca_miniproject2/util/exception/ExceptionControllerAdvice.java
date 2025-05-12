e8ea2dc8f6022f4f545f13bb86016c4f7779bba8
Jang-GO <jang8195@naver.com>
2025-05-12 17:13:23 +0900

    [FEAT] : 사용자 정의 예외 처리

--- src/main/java/ureca/ureca_miniproject2/util/exception/ExceptionControllerAdvice.java
+++ src/main/java/ureca/ureca_miniproject2/util/exception/ExceptionControllerAdvice.java
@@ -4,8 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ureca.ureca_miniproject2.util.exception.custom.ForbiddenException;
 import ureca.ureca_miniproject2.util.exception.custom.ImageUploadException;
 import ureca.ureca_miniproject2.util.exception.custom.NotFoundException;
+import ureca.ureca_miniproject2.util.exception.custom.UnAuthorizedException;
 import ureca.ureca_miniproject2.util.response.ApiResponse;
 
 @RestControllerAdvice
@@ -24,5 +26,15 @@ public class ExceptionControllerAdvice {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e) {
         return ApiResponse.fail(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    }
+
+    @ExceptionHandler(UnAuthorizedException.class)
+    public ResponseEntity<?> handleUnAuthorizedException(UnAuthorizedException e) {
+        return ApiResponse.fail(HttpStatus.UNAUTHORIZED.value(), e.getMessage());
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<?> handleForbiddenException(ForbiddenException e) {
+        return ApiResponse.fail(HttpStatus.FORBIDDEN.value(), e.getMessage());
     }
 }
---
+++ src/main/java/ureca/ureca_miniproject2/util/exception/custom/UnAuthorizedException.java
@@ -0,0 +1,7 @@
+package ureca.ureca_miniproject2.util.exception.custom;
+
+public class UnAuthorizedException extends RuntimeException {
+    public UnAuthorizedException(String message) {
+        super(message);
+    }
+}
